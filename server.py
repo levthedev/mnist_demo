2b979084eed05220e3527c59375b15fea183e911
Lev Kravinsky <kravinskylev@gmail.com>
2017-04-12 14:44:26 -0400

    add cors

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 import numpy as np
 from improved_network import *
 
 app = Flask(__name__)
+CORS(app)
 net = load('./saved3')
 
 @app.route("/")
