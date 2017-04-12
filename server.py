a67959dbdc78ea047f16528f64bbe04e3fb1d231
Lev Kravinsky <kravinskylev@gmail.com>
2017-04-12 14:35:27 -0400

    only load net once

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 import numpy as np
 from improved_network import *
-# import pdb
 
 app = Flask(__name__)
-# app.debug = True
+net = load('./saved3')
 
 @app.route("/")
 def root():
@@ -14,7 +13,6 @@ def root():
 def process():
     dirty_input = request.args['vector'].encode('latin_1').split(',')
     clean_input = np.reshape(np.array(map(float, dirty_input)), (784, 1))
-    net = load('./saved3')
     vector_result = np.hstack(net.feedforward(clean_input))
     probabilities = zip(range(0, 10), vector_result.tolist())
     scaled = [(t[0], "{0:.2f}".format(round(t[1] * 100, 2))) for t in probabilities]
@@ -22,5 +20,4 @@ def process():
     return jsonify({"result": result})
 
 if __name__ == "__main__":
-    # app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
